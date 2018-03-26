53dd97f61b47798fa8eac75d4239e8e5667e28d9
adrien <zaffran.ad@gmail.com>
2018-03-26 22:47:04 +0200

    enhance routing

--- src/main/kotlin/zaf/stock/stockportfolio/PortfolioHandler.kt
+++ src/main/kotlin/zaf/stock/stockportfolio/PortfolioHandler.kt
@@ -2,25 +2,69 @@ package zaf.stock.stockportfolio
 
 import org.slf4j.LoggerFactory
 import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.*
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyExtractors
+import org.springframework.web.reactive.function.BodyExtractors.*
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.*
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
+import javax.sound.sampled.Port
 
 @Component
 class PortfolioHandler(val portfolioRepository: PortfolioRepository) {
 
+    val log = LoggerFactory.getLogger(PortfolioHandler::class.java.name)
+
     companion object {
-        val LOG = LoggerFactory.getLogger(PortfolioHandler::class.java.name)
+        val namePathVariable = "portfolioName"
     }
 
-    fun getPortfolio(name: String): Portfolio? = try {
-        portfolioRepository.findByName(name)
-    } catch (e: EmptyResultDataAccessException) {
-        LOG.debug("portfolio not found with name $name : $e")
-        null
+    /**
+     * Retrieve the portfolio with name $name
+     * if found, the portfolio is returned
+     * if not, 404 returned
+     */
+    fun getPortfolio(request: ServerRequest): Mono<ServerResponse> {
+        val name = request.pathVariable(namePathVariable)
+        return try {
+            toServerResponse(portfolioRepository.findByName(name))
+        } catch (e: EmptyResultDataAccessException) {
+            log.debug("portfolio not found with name $name - $e")
+            notFound().build()
+        }
     }
 
-    fun createPortfolio(name: String): Mono<Portfolio> = portfolioRepository.save(
-            Portfolio(name = name)
-    ).toMono()
+    fun createPortfolio(request: ServerRequest): Mono<ServerResponse> {
+        val formToPortfolio = formToPortfolio(request)
+
+        return try {
+            val portfolioSaved = portfolioRepository.save(formToPortfolio)
+            toServerResponse(portfolioSaved)
+        } catch (e: Exception) {
+            log.debug("error while saving portfolio: $e")
+            status(NOT_MODIFIED).build()
+        }
+    }
+
+    fun formToPortfolio(request: ServerRequest): Portfolio{
+        val name = request.body(toFormData()).map {
+            it.toSingleValueMap()["name"]
+        }.toString()
+
+        return Portfolio(name = name)
+    }
+
+    private fun toServerResponse(portfolioSaved: Portfolio): Mono<ServerResponse> {
+        ok().body(
+                BodyInserters.fromObject(
+                        portfolioSaved
+                )
+        )
+    }
+
 }
--- src/main/kotlin/zaf/stock/stockportfolio/RoutingConfiguration.kt
+++ src/main/kotlin/zaf/stock/stockportfolio/RoutingConfiguration.kt
@@ -2,12 +2,10 @@ package zaf.stock.stockportfolio
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.function.BodyExtractors
-import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
+import zaf.stock.stockportfolio.PortfolioHandler.Companion.namePathVariable
 
 @Configuration
 class RoutingConfiguration {
@@ -16,29 +14,13 @@ class RoutingConfiguration {
     fun routerFunction(handler: PortfolioHandler) : RouterFunction<ServerResponse> = router {
 
         ("/portfolios").nest {
-            val namePathVariable = "portfolioName"
 
             GET("/portfolio/{$namePathVariable}"){
-                val name = it.pathVariable(namePathVariable)
-
-                val portfolio = handler.getPortfolio(name)
-
-                if(portfolio == null){
-                    ServerResponse.notFound().build()
-                }else{
-                    ServerResponse.ok().body(BodyInserters.fromObject(portfolio))
-                }
+                handler.getPortfolio(it)
             }
 
             POST("/portfolio"){
-                val name = it.body(BodyExtractors.toFormData()).map {
-                  it.toSingleValueMap().get("name")
-
-                }.toString()
-
-                ServerResponse.ok().body(
-                        handler.createPortfolio(name)
-                )
+                handler.createPortfolio(it)
             }
         }
     }
