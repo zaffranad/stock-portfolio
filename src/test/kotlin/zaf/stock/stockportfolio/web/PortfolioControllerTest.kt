9241806c76a574b746ab01e2d0699be45dc15abf
adrien <zaffran.ad@gmail.com>
2018-05-06 00:44:26 +0200

    ajout tests unitaires portfolio controller

--- src/test/kotlin/zaf/stock/stockportfolio/web/PortfolioControllerTest.kt
+++ src/test/kotlin/zaf/stock/stockportfolio/web/PortfolioControllerTest.kt
@@ -1,104 +1,62 @@
 package zaf.stock.stockportfolio.web
 
-import org.assertj.core.api.Assertions
+import org.hamcrest.CoreMatchers
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.core.ParameterizedTypeReference
-import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit4.SpringRunner
-import zaf.stock.stockportfolio.portfolio.exception.PortfolioOperationException
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import zaf.stock.stockportfolio.portfolio.facade.PortfolioFacade
 import zaf.stock.stockportfolio.portfolio.model.Portfolio
 
 @RunWith(SpringRunner::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class PortfolioControllerTest {
-
-    private final inline fun <reified T : Any> typeRef(): ParameterizedTypeReference<T> = object : ParameterizedTypeReference<T>() {}
-
-    @LocalServerPort
-    var port: Int = 0
+@WebMvcTest
+class PortfolioControllerTest{
 
     @Autowired
-    lateinit var testRestTemplate: TestRestTemplate
+    lateinit var mockMvc: MockMvc
 
     @MockBean
     lateinit var portfolioFacade: PortfolioFacade
 
     @Test
-    fun getAllPortfolios_Should_return_empty_list_when_no_portfolios_exists() {
-
-        // given
-        Mockito.`when`(portfolioFacade.getAll()).thenReturn(emptyList())
-
-        // when
-        val results = testRestTemplate.getForEntity(
-                "http://localhost:$port/portfolios/",
-                List::class.java
-        )
-
-        //then
-        Assertions.assertThat(
-                results.statusCode
-        ).isEqualTo(HttpStatus.OK)
-
-        Assertions.assertThat(
-                results.body
-        ).isEmpty()
-    }
-
-    @Test
-    fun getAllPortfolios_Should_return_all_portfolios_when_portfolios_exists() {
-
-        // given
-        val name1 = "nnn"
-        val name2 = "ooo"
-        Mockito.`when`(portfolioFacade.getAll()).thenReturn(listOf(
-                Portfolio(name1),
-                Portfolio(name2)
-        ))
-
-        // when
-        val results = testRestTemplate.getForEntity(
-                "http://localhost:$port/portfolios/",
-                List::class.java
-        )
-
-        //then
-        Assertions.assertThat(
-                results.statusCode
-        ).isEqualTo(HttpStatus.OK)
-
-        Assertions.assertThat(
-                results.body
-        ).isNotEmpty
-                .extracting("name")
-                .containsExactly(name1, name2)
+    fun getAll_Should_return_200_and_empty_list_when_no_portfolio_found() {
+        this.mockMvc.perform(get("/portfolios/"))
+                    .andDo(print())
+                    .andExpect(status().isOk)
+                    .andExpect(MockMvcResultMatchers.content().string("[]"))
 
     }
 
     @Test
-    fun getAllPortfolios_Should_return_5OO_when_exception_while_processing() {
+    fun getAll_Should_return_200_and_list_of_portfolio_when_portfolios_found() {
 
-        // given
-        Mockito.`when`(portfolioFacade.getAll()).thenThrow(PortfolioOperationException())
+        Mockito.`when`(portfolioFacade.getAll())
+                .thenReturn(
+                        listOf(
+                                Portfolio("ptf1"),
+                                Portfolio("ptf2")
+                        )
+                )
 
-        // when
-        val response = testRestTemplate.getForEntity(
-                "http://localhost:$port/portfolios/",
-                Any::class.java
-        )
+        this.mockMvc.perform(get("/portfolios/"))
+                    .andDo(print())
+                    .andExpect(status().isOk)
+                    .andExpect(
+                            MockMvcResultMatchers.content()
+                                    .string(CoreMatchers.allOf(
+                                            CoreMatchers.containsString("ptf1"),
+                                            CoreMatchers.containsString("ptf2")
 
-        //then
-        Assertions.assertThat(
-                response.statusCode
-        ).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR)
+                                    ))
+                    )
 
     }
 }
