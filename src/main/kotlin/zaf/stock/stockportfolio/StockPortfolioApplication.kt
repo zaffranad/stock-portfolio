b5a100ce5a3ea582ce8cb56e4cdd5cfa08a0da34
adrien <zaffran.ad@gmail.com>
2018-05-05 14:24:13 +0200

    ajout swagger

--- src/main/kotlin/zaf/stock/stockportfolio/StockPortfolioApplication.kt
+++ src/main/kotlin/zaf/stock/stockportfolio/StockPortfolioApplication.kt
@@ -2,7 +2,9 @@ package zaf.stock.stockportfolio
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+@EnableSwagger2
 @SpringBootApplication
 class StockPortfolioApplication
 
